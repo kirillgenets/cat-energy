f2508a25ac90601b54a4feb639c3837aac74ebaf
Kirill <12ab8192>
2019-05-20 21:02:48 +0300

    fixed bug with index.html (not all)

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,14 @@
-let navToggler = document.getElementsByClassName('nav__toggler')[0];
-let navList = document.getElementsByClassName('nav__list')[0];
+let navToggler = document.querySelector('.nav__toggler');
+let navList = document.querySelector('.nav__list');
 
 navToggler.classList.remove('nav__toggler--no-js');
 navToggler.classList.add('nav__toggler--closed-menu');
 
 navList.classList.add('nav__list--closed');
 
-navToggler.addEventListener('click', function() {
+navToggler.onclick = () => {
 
 	navList.classList.toggle('nav__list--closed');
 	navToggler.classList.toggle('nav__toggler--closed-menu')
 
-});
+};
