423593385bad1f7eb5ac2d988e93142cd1e059fd
Kirill Genets <kirill.genets2002@mail.ru>
2019-06-02 12:20:56 +0300

    saved half-working slider

--- www/js/slider.js
+++ www/js/slider.js
@@ -2,10 +2,21 @@ let toggleButtonBefore = document.querySelector('.example__button--before');
 let toggleButtonAfter = document.querySelector('.example__button--after');
 
 let catBefore = document.querySelector('.example__cat--before');
-
+let catAfter = document.querySelector('.example__cat--after');
 
 toggleButtonBefore.addEventListener('click', function() {
 
-	catBefore.style.clip = 'rect(auto,690px,auto,auto)';
+	catAfter.classList.remove('example__cat--shown');
+	catBefore.classList.remove('example__cat--hidden');
+	catBefore.classList.add('example__cat--shown');
+
+
+});
+
+toggleButtonAfter.addEventListener('click', function() {
+
+	catBefore.classList.remove('example__cat--shown');
+	catBefore.classList.add('example__cat--hidden');
+	catAfter.classList.add('example__cat--shown');
 
 });
