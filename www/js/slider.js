b0184f1387b397b6a8b6d70b12141883e24de3ca
Kirill Genets <kirill.genets2002@mail.ru>
2019-06-02 12:23:59 +0300

    normal master state

--- www/js/slider.js
+++ www/js/slider.js
@@ -1,11 +1,11 @@
-let toggleButtonBefore = document.querySelector('.example__button--before');
-let toggleButtonAfter = document.querySelector('.example__button--after');
+// let toggleButtonBefore = document.querySelector('.example__button--before');
+// let toggleButtonAfter = document.querySelector('.example__button--after');
 
-let catBefore = document.querySelector('.example__cat--before');
+// let catBefore = document.querySelector('.example__cat--before');
 
 
-toggleButtonBefore.addEventListener('click', function() {
+// toggleButtonBefore.addEventListener('click', function() {
 
-	catBefore.style.clip = 'rect(auto,690px,auto,auto)';
+// 	catBefore.style.clip = 'rect(auto,690px,auto,auto)';
 
-});
+// });
